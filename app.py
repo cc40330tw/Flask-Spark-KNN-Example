57d02408dc0c05587edab57002a92c7d6b3a9ebe
cc40330tw <cc40330tw@yahoo.com.tw>
2021-01-08 17:51:26 +0800

    plus login

--- app.py
+++ app.py
@@ -1,14 +1,27 @@
-from flask import Flask, render_template, url_for, jsonify, request
+from flask import Flask, render_template, url_for, jsonify, request, redirect, session, flash
 #import test
 import final
+from datetime import timedelta
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.secret_key = "hellothisismysecretkey"
 app.config['JSON_AS_ASCII'] = False
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:admin@localhost:3306/traindb"
+#app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:admin@localhost:3306/traindb"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users_table.sqlite3' # "users_table" here is the name of the table that you're gonna be referencing 
+app.permanent_session_lifetime = timedelta(minutes=3)
 db = SQLAlchemy(app)
+
+class users_table(db.Model): # The columns represent pieces of information；Rows represent in ；Rows represent individual items
+    _id = db.Column("id",db.Integer, primary_key=True) # id will be automatically be created for us because it's a primary key
+    name = db.Column(db.String(100)) # 100 here is the maximum length of the string that we want to store(100 characters)
+    email = db.Column(db.String(100)) # string也可以改成integer/float/boolean
+
+    def __init__(self,name,email): # We want to store users and each users has a name and an email (these 2 are what we need every time we define a new user object)(the init method will take the variables that we need to create a new object)
+        self.name = name
+        self.email = email
 
 class Knn(db.Model):
     rid = db.Column(db.Integer, primary_key=True)
@@ -35,11 +48,77 @@ class Knn(db.Model):
         #self.query.filter_by(rid= _rid).first()
         self.query.all()
 
-    
+
 
 @app.route('/')
+def indexindex():
+    return render_template('indexindex.html')
+
+@app.route('/index')
 def index():
     return render_template('index.html')
+
+@app.route("/login",methods=["POST","GET"])
+def login():
+    if request.method == "POST":
+        session.permanent = True #used to define this specific session as a permanent session which means it's gonna last as long as we define up there 
+        user = request.form["nm"]
+        session["user"] = user
+        found_user = users_table.query.filter_by(name=user).first()
+        if found_user: # When an user types his name, we'll check if this user is already exist. If not then we'll create one
+            session["email"] = found_user.email
+        else:
+            usr = users_table(user, "")
+            db.session.add(usr) # add this user model to our database
+            db.session.commit()
+
+        flash("Login Succesful!")
+        return redirect(url_for("user"))
+    else:
+        if "user" in session: #代表若已經是signed in的狀態
+            flash("Already Logged in!")
+            return redirect(url_for("user"))
+        return render_template("login.html")
+
+@app.route("/user",methods=["POST","GET"])
+def user():
+    email = None
+    if "user" in session:
+        user = session["user"]
+        if request.method == "POST": 
+            email = request.form["email"] # grab that email from the email field
+            session["email"] = email # store it in the session
+                   
+            found_user = users_table.query.filter_by(name=user).first()
+            found_user.email = email
+            db.session.commit() # next time we login this will be saved
+            flash("Email was saved!")
+            return redirect(url_for("index"))
+
+        else: # if it's a GET request
+            if "email" in session:
+                email = session["email"] # get the email from the session
+        return render_template("User.html", email=email)
+    else:
+        flash("You are not logged in!")
+        return redirect(url_for("login"))
+
+@app.route("/logout")
+def logout():
+    #if "user" in session:
+    #user = session["user"]
+    flash("You have been logged out!", "info")
+    session.pop("user",None) #remove the user data from my session 
+    session.pop("email",None)
+    return redirect(url_for("login"))
+
+@app.route("/view")
+def view():
+    return render_template("view.html",values=users_table.query.all())
+
+@app.route("/About")
+def About():
+    return render_template("new.html")
 
 @app.route('/train', methods=['POST'])
 def translate_text():
@@ -69,8 +148,10 @@ def query_all_data():
     return jsonify(response)
 
 
+
 if __name__ == '__main__':
-	app.run(host='0.0.0.0')
-	
+    #db.create_all()
+    #app.run(host='0.0.0.0')
+    app.run(debug=True)
 	#print(type(o.rid), type(o.distance), type(o.score), type(o.neighbor), type(o.datasetName), type(o.featureLen), type(o.timestamp))
 	
